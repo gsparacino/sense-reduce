ef1a2a49dc3419d37f55ba80ded43aa02b61bd0b
gsparacino <25706462+gsparacino@users.noreply.github.com>
2024-06-16 14:07:43 +0200

    feat: add MultivariateDataframeMockSensor that reads measurements from DataFrames

--- sensor/multivariate_sensor_mock_csv.py
+++ sensor/multivariate_sensor_mock_csv.py
@@ -2,11 +2,12 @@ import logging
 import os
 
 import pandas as pd
+from pandas import DataFrame
 
-from abstract_sensor import AbstractSensor
+from sensor.multivariate_sensor_mock_dataframe import MultivariateDataframeMockSensor
 
 
-class MultivariateCsvMockSensor(AbstractSensor):
+class MultivariateCsvMockSensor(MultivariateDataframeMockSensor):
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
     def __init__(self, csv_path: str) -> None:
@@ -15,17 +16,18 @@ class MultivariateCsvMockSensor(AbstractSensor):
 
         :param csv_path: the relative path to the CSV file containing the sensor data.
         """
-        super().__init__()
+        super().__init__(self.read_csv(csv_path))
         # TODO this list should always match the model's input layer, make the coupling more explicit?
         #  i.e. the Sensor could receive an array of features upon registration to the BS
         self.features = ['TL', 'P', 'RF', 'SO', 'RR', 'DD']
+
+    def read_csv(self, csv_path: str) -> DataFrame:
         if csv_path:
             path = os.path.join(self.BASEDIR, csv_path)
             logging.debug(f"Loading sensor mock data {path}")
             df = pd.read_csv(path)
             df.reset_index(inplace=True)
-            self.data = df
-            self.iterator = df.iterrows()
+            return df
         else:
             raise ValueError("MultivariateCsvMockSensor requires a valid path ta a CSV file as input")
 
---
+++ sensor/multivariate_sensor_mock_dataframe.py
@@ -0,0 +1,34 @@
+import os
+
+import pandas as pd
+from pandas import DataFrame
+
+from abstract_sensor import AbstractSensor
+
+
+class MultivariateDataframeMockSensor(AbstractSensor):
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+    def __init__(self, df: DataFrame) -> None:
+        """
+        A multivariate sensor that returns pre-defined data, loaded from a Pandas' DataFrame.
+
+        :param data: the Dataframe containing the sensor data.
+        """
+        super().__init__()
+        # TODO this list should always match the model's input layer, make the coupling more explicit?
+        #  i.e. the Sensor could receive an array of features upon registration to the BS
+        self.features = ['TL', 'P', 'RF', 'SO', 'RR', 'DD']
+        self.data = df
+        self.iterator = df.iterrows()
+
+    @property
+    def measurement(self) -> pd.Series:
+        try:
+            return self.nextItem()
+        except StopIteration:
+            self.iterator = self.data.iterrows()
+            return self.nextItem()
+
+    def nextItem(self):
+        return next(self.iterator)[1][self.features]
